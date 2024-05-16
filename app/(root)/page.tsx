c5abba0751a60f4faed78c40a00a04dbe7a9c611
TariqKichawele <tariqkichawele66@gmail.com>
2024-05-16 14:58:45 -0400

    Implement complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,45 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
+import RightSidebar from '@/components/RightSidebar'
 import React from 'react'
 
 const Home = () => {
+    const loggedIn = { firstName: "Tariq", lastName: "Kichawele", email: "TK@gmail.com" };
+
   return (
-    <div>Home</div>
+    <section className='home'>
+        <div className='home-content'>
+            <header className='home-header'>
+                <HeaderBox 
+                    type="greeting"
+                    title="Welcome"
+                    user={'Guest'}
+                    subtext="Access and manage your account and transactions efficiently"
+                />
+
+                <TotalBalanceBox 
+                    accounts={[]}
+                    totalBanks={1}
+                    totalCurrentBalance={1000}
+                />
+            </header>
+
+            RECENT TRANSACTIONS
+        </div>
+
+        <RightSidebar 
+            user={loggedIn}
+            transactions={[]}
+            banks={[
+                {
+                    currentBalance: 123.50
+                }, 
+                {
+                    currentBalance: 500.50
+                }
+            ]}
+        />
+    </section>
   )
 }
 
